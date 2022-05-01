94b69ae5e1d071d8f336ddd981237128438876d3
andy <andyqe@gmail.com>
2022-05-01 10:41:05 -0700

    error check update

--- cpp_inputs.cpp
+++ cpp_inputs.cpp
@@ -3,24 +3,40 @@
 double get_double()
 {
     string input;
-    while(!(cin >> input) || !is_decimal_str(input))
+    try
     {
-        cout << "is not double\nagain: ";
-        cin.clear();
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        while(!(cin >> input) || !is_decimal_str(input))
+        {
+            cout << "is not double\nagain: ";
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
     }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
     return stod(input);
 }
 
 uint64_t get_64int()
 {
     string input;
-    while(!(cin >> input) || !is_int_str(input))
+    try
     {
-        cout << "is not a integer\nagain: ";
-        cin.clear();
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        while(!(cin >> input) || !is_int_str(input))
+        {
+            cout << "is not a integer\nagain: ";
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
     }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
     return stoul(input);
 }
 
@@ -38,6 +54,10 @@ string get_str()
 
 bool is_decimal_str(string input)
 {
+    if (input.empty())
+    {
+        throw std::runtime_error("empty argument to is_decimal_str()");
+    }
     int dec = 0;
     int index = 0;
     for (auto ch : input)
@@ -53,6 +73,10 @@ bool is_decimal_str(string input)
 
 bool is_int_str(string input)
 {
+    if (input.empty())
+    {
+        throw std::runtime_error("empty argument to is_int_str()");
+    }
     for (auto ch : input)
     {
         if (!isdigit(ch)) { return false; }
